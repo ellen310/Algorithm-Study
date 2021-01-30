6214d75206125e01ad332348633b4bc771a7c6c9
yeji <yejih310@gmail.com>
2021-01-30 18:10:41 +0900

    Create 1026.java

---
+++ BaekJoon/1026.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int min=0;
+		
+		int[] a=new int[n];
+		for(int i=0;i<n;i++) a[i]=sc.nextInt();
+		
+		int[] b=new int[n];
+		for(int i=0;i<n;i++) b[i]=sc.nextInt();
+		
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		for(int i=0;i<n;i++) {
+			min += a[i]*b[n-1-i];
+		}
+		System.out.println(min);
+	}
+
+}
